236aff4d30c69d0c9726d9c27338d3f7674069d9
kedharnathrafcos <kedharnath22@gmail.com>
2020-09-18 08:34:05 +0530

    firsrt coomm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngSlick';
+  title = 'ngSlick'; 
 
 
   slides = [1, 2, 3, 4, 5, 6, 7, 8];
